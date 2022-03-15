3341732beb4ca7c2f5bc5ae28aa074efdbd757c5
Théo B <theo.brunet@insa-cvl.fr>
2022-03-15 10:40:00 +0100

    Ajout du client react

---
+++ expressjs-starter-kit-master/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+	router = express.Router(),
+	indexController = require('../controllers');
+
+router.route('/')
+	.get(indexController.dashboard);
+
+router.use('/users', require('./users'));
+
+module.exports = router;
