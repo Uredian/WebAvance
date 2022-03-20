a8496b9765f942fda90978a447e1a6dffc473def
Alyson <aoustinalyson@gmail.com>
2022-03-20 14:02:03 +0100

    nettoyage modele user

--- server/models/users.js
+++ server/models/users.js
@@ -1,57 +1,57 @@
+//Importing the database
 var dbConn = require("./db.js");
 
-var Users = function (user){
+//Definition of a user
+var User = function (user) {
 	this.Id = user.Id;
-    this.Nom=user.Nom
-    this.Prenom=user.Prenom
-    this.Email=user.Email
-    this.Adresse_numero=user.Adresse_numero
-    this.Adresse_rue=user.Adresse_rue
-    this.Ville=user.Ville
-    this.Code_postal=user.Code_postal
+	this.Nom = user.Nom
+	this.Prenom = user.Prenom
+	this.Email = user.Email
+	this.Adresse_numero = user.Adresse_numero
+	this.Adresse_rue = user.Adresse_rue
+	this.Ville = user.Ville
+	this.Code_postal = user.Code_postal
 }
 
-Users.findAll = function (result){
-	let query=dbConn.query("SELECT * FROM Utilisateur",function(err,res){
-		if (err){
-			console.log("error = ",err)
-			result(null,err)
+//Method to find all users
+User.findAll = function (result) {
+	let query = dbConn.query("SELECT * FROM Utilisateur", function (error, response) {
+		if (error) {
+			console.log("error = ", error)
+			result(null, error)
 		}
-		else{
-			
-		result(null,res)
+		else {
+			result(null, response)
 		}
-		
 	})
 }
 
-Users.create = function ( newuser, result){
-    let query=dbConn.query("INSERT INTO Utilisateur set ?",newuser,function(err,res){
-		if (err){
-			console.log("error = ",err)
-			result(null,err)
+//Method to create a user 
+User.create = function (newuser, result) {
+	let query = dbConn.query("INSERT INTO Utilisateur set ?", newuser, function (error, response) {
+		if (error) {
+			console.log("error = ", error)
+			result(null, error)
 		}
-		else{
-			newuser.Id=res.insertId
-		result(null,newuser)
+		else {
+			newuser.Id = res.insertId
+			result(null, newuser)
 		}
-		
-		
 	})
 }
 
-Users.FindById= function (id,result){
-	let query_get_quantity=dbConn.query("SELECT * FROM Utilisateur WHERE Id=?",[id],function(err,res){
-		if (err){
-			console.log("error = ",err)
-			result(null,err)
+//Method to find a user by id
+User.FindById = function (id, result) {
+	let query_get_quantity = dbConn.query("SELECT * FROM Utilisateur WHERE Id=?", [id], function (error, response) {
+		if (error) {
+			console.log("error = ", error)
+			result(null, error)
 		}
-		else{
-			result(null,res)
+		else {
+			result(null, response)
 		}
-		
 	})
 }
 
 
-module.exports= Users;
+module.exports = User;
